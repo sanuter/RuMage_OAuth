a660a9c7b884ed046c686212af3306119aeb45bf
A.Treitjak <a.treitjak@gmail.com>
2014-01-08 11:07:16 +0300

    Added service helper

---
+++ app/code/community/RuMage/OAuth/Helper/Service.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @package Magento OAuth.
+ * @author: A.A.Treitjak
+ * @copyright: 2012 - 2014 BelVG.com
+ */
+
+class RuMage_OAuth_Helper_Service extends Mage_Core_Helper_Abstract
+{
+
+} 
