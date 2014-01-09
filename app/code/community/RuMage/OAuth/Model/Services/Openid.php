7b5574d3193abb533977c1a0ee325829e7f2d5a1
A.Treitjak <a.treitjak@gmail.com>
2014-01-09 17:04:50 +0300

    Update providers.

---
+++ app/code/community/RuMage/OAuth/Model/Services/Openid.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package Magento OAuth.
+ * @author: A.A.Treitjak
+ * @copyright: 2012 - 2014 BelVG.com
+ */
+
+
+class RuMage_OAuth_Model_Services_Openid extends RuMage_OAuth_Model_Service
+{
+    public function _construct()
+    {
+        $this->setServiceName('openid');
+        $this->setClientIdKey('key');
+        $this->setClientSecretKey('secret');
+    }
+}
